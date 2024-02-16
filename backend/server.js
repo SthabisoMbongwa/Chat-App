0a1221e3e0b4f76fccdca124a6271b7ab853a911
Sthabiso <sthabiso@aims.ac.za>
2024-02-16 17:47:01 +0200

    Started the server, and started listening for incomming request

--- backend/server.js
+++ backend/server.js
@@ -2,5 +2,8 @@ const express = require('express');
 
 const app = express();
 
-const PORT = 3000;
+const PORT = 5000;
 
+app.listen(PORT, () => {
+    console.log("Server is running on port ", `${PORT}`)
+});
