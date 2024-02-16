43184b45278e2ea18f50ffcdc716e5faee6bcf36
Sthabiso <sthabiso@aims.ac.za>
2024-02-16 17:53:13 +0200

    Changed the script in package.json to nodemon server.js

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,10 @@ const app = express();
 
 const PORT = 5000;
 
+app.get("/", (req, res) => {
+    res.send("Hello world");
+})
+
 app.listen(PORT, () => {
     console.log("Server is running on port ", `${PORT}`)
 });
