99ee7e72e645d60e9649c646bc801f1419ee442c
Sthabiso <sthabiso@aims.ac.za>
2024-02-17 20:35:36 +0200

    Imported and configured the .env file so that I can get the app settings stored in there.

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const dotenv = require("dotenv");
+dotenv.config();
 
 const app = express();
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
     res.send("Hello world");
