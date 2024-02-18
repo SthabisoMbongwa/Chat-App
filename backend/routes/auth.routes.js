eb3f6d628a262233d04e063d2f7a5fc00bc67caa
Sthabiso <sthabiso@aims.ac.za>
2024-02-18 20:03:07 +0200

    Created a post http method to register users

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -6,4 +6,8 @@ router.get("/login", (req, res) => {
     res.send("Login Route");
 });
 
+router.post('/register', (req, res) => {
+    res.send("You have registered successfully");
+})
+
 export default router;
