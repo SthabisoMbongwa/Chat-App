2da16eacb64d4361024040b6ce77d1d88b841eee
Sthabiso <sthabiso@aims.ac.za>
2024-02-20 21:28:11 +0200

    Added a signup router

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -2,11 +2,15 @@ import express from "express";
 
 const router = express.Router();
 
+router.get("/signup", (req, res) => {
+    res.send("Login Route");
+});
+
 router.get("/login", (req, res) => {
     res.send("Login Route");
 });
 
-router.post('/register', (req, res) => {
+router.get('/register', (req, res) => {
     res.send("You have registered successfully");
 })
 
