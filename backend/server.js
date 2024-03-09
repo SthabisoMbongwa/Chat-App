d8a245ee12056be981c78db3e9d97b25b56611ae
Sthabiso <sthabiso@aims.ac.za>
2024-03-09 19:29:51 +0200

    Created a models folder and user.model file that will house the usermodel.

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import authRoutes from './routes/auth.routes.js';
+import connectToMongoDB from "./db/connectToMongoDB.js";
 
 dotenv.config();
 
@@ -15,5 +16,6 @@ app.get("/", (req, res) => {
 app.use("/api/auth", authRoutes)
 
 app.listen(PORT, () => {
-    console.log("Server is running on port ", `${PORT}`)
+    connectToMongoDB();
+    console.log("Server is running on port ", `${PORT}`);
 });
