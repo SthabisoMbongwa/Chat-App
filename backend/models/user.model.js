88197bda3fad36ee1c7222a74d530a71d84b80f7
Sthabiso <sthabiso@aims.ac.za>
2024-03-09 19:36:23 +0200

    Created a userSchema

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,5 +1,28 @@
 import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema({
-    
-})
+    fullName:{
+        type:String,
+        required:true
+    },
+    username:{
+        type:String,
+        ruquired:true,
+        unique:true,
+    },
+    password: {
+        type: String,
+        required: true,
+        minlengthe:6,
+    },
+    gender:{
+        type: String,
+        ruquired: true,
+        enum: ["male", "female"],
+    },
+    profilePic: {
+        type: String,
+        default: "",
+    },
+});
+
