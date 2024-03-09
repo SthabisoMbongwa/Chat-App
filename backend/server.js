6edd1fddef67b801f736f43013346ea5ad54b202
Sthabiso <sthabiso@aims.ac.za>
2024-03-09 19:53:23 +0200

    Edded express.json middleware, to parse the incoming requests with JSON payloads.

--- backend/server.js
+++ backend/server.js
@@ -3,17 +3,20 @@ import dotenv from "dotenv";
 import authRoutes from './routes/auth.routes.js';
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
-dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5000; 
 
-const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-    res.send("Hello world");
-})
+dotenv.config();
 
+app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
 app.use("/api/auth", authRoutes)
+
+// app.get("/", (req, res) => {
+//     res.send("Hello world");
+// })
+
 
 app.listen(PORT, () => {
     connectToMongoDB();
