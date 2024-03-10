fe12821057a94800aca7a0aa66a57e4681fd8542
Sthabiso <sthabiso@aims.ac.za>
2024-03-10 21:14:24 +0200

    Created a user and saved them to the database.

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -13,15 +13,28 @@ export const signup = async (req, res) => {
         if(user){
             return res.status(400).json({error: "Username already exists"})
         }
+
+                
+        const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`
+        const girlProfilePic =`https://avatar.iran.liara.run/public/girl?username=${username}`
+
+        const newUser = new User({
+            fullName,
+            username,
+            password,
+            gender,
+            profilePic: gender === "male" ? boyProfilePic : girlProfilePic
+        })
+
+        await newUser.save();
+
+        res.status(201);
        
     } catch (error){
 
     }
 };
 
-
-const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`
-const girlProfilePic =`https://avatar.iran.liara.run/public/girl?username=${username}`
 
 
 export const login = (req, res) => {
