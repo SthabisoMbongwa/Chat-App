6feb4a393d243ea83bf7b11980395fd81744a039
Sthabiso <sthabiso@aims.ac.za>
2024-03-13 16:58:24 +0200

    Added a res status with code 200 and a json body for user who loggedin successfully.

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -64,6 +64,15 @@ export const login = async (req, res) => {
         if(!user || !isPasswordCorrect){
             return res.status(400).json({error: "Invalid username or password"});
         }
+
+        generateTokenAndSetCookie(user._id, res);
+
+        res.status(200).json({
+            _id: user._id,
+            fullName: user.fullName,
+            username: user.username,
+            profilePic: user.profilePic,
+        });
     }catch(error){
         console.log("Error in login controller", error.message);
         res.status(500).json({error: "Internal Server Error"});
