65dead4680f7e9e681abc284e7a08ed2d9d56074
Sthabiso <sthabiso@aims.ac.za>
2024-03-13 16:48:05 +0200

    Added a condition to return status 400  if the user does not exist.

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -59,6 +59,11 @@ export const login = async (req, res) => {
     try{
         const {username, password} = req.body;
         const user = await User.findOne({username});
+        const isPasswordCorrect = await bcrypt.compare(password, user?.password || "");
+
+        if(!user || !isPasswordCorrect){
+            return res.status(400).json({error: "Invalid username or password"});
+        }
     }catch(error){
         console.log("Error in login controller", error.message);
         res.status(500).json({error: "Internal Server Error"});
