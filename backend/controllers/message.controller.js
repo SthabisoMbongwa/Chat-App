acf26eef0551eb5c26a5bc9e2633baf748430001
Sthabiso <sthabiso@aims.ac.za>
2024-03-15 21:03:31 +0200

    Used findOne to find a conversation between participants, using sendeId and receiverId.

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -1,8 +1,12 @@
 export const sendMessage = async (req, res) => {
     try{
         const {message} = req.body;
-        const { id } = req.params;   //or const id = req.params.id
-        const senderId = req.userId
+        const { id: receiverId } = req.params;   //or const id = req.params.id
+        const senderId = req.user._id
+
+        await Conversation.findOne({
+            participants: {$all: [senderId, receiverId] },
+        })
     }catch(error){
         console.log("Error in sendMessage controller: ", error.message);
         res.status(500).json({error: "Internal server error"});
