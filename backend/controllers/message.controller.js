d787f7a6d9a8c8f109eb222dbfd9fc41e39784e1
Sthabiso <sthabiso@aims.ac.za>
2024-03-15 21:06:59 +0200

    Added a condition to create a conversation between users if it does not exist.

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -4,9 +4,15 @@ export const sendMessage = async (req, res) => {
         const { id: receiverId } = req.params;   //or const id = req.params.id
         const senderId = req.user._id
 
-        await Conversation.findOne({
+        let conversation = await Conversation.findOne({
             participants: {$all: [senderId, receiverId] },
         })
+
+        if(!conversation){
+            conversation = await Conversation.create({
+                participants: [senderId, receiverId],
+            })
+        }
     }catch(error){
         console.log("Error in sendMessage controller: ", error.message);
         res.status(500).json({error: "Internal server error"});
